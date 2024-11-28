7bc1ead207317d0f92f7aec8a379bb2a2260784a
Abubakr <133663527+bakrcode@users.noreply.github.com>
2024-11-28 19:40:43 +0200

    Add files via upload

---
+++ assets/use-timeout.f75264b7.js
@@ -0,0 +1,1 @@
+import{f as r}from"./vm.da018064.js";import{N as i,u as m,g as s}from"./index.182575de.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,u){t(),r(o)===!1&&(e=setTimeout(()=>{e=null,n()},u))}}}export{f as u};
